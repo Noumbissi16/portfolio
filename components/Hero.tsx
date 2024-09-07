501a773beb4af8092c97d10abb98c2f20c9f33f3
Noumbissi16 <noumbissistael@gmail.com>
2024-09-07 21:29:57 +0100

    Finish v1.0

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-36 h-screen" id="hero">
       {/**
        *  UI: Spotlights
        *  Link: https://ui.aceternity.com/components/spotlight
